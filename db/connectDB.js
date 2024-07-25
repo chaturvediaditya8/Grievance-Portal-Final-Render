7a4c4db2466b6bec45a5cff09c07a0ef1e2954fa
chaturvediaditya8 <chaturvediaditya8@gmail.com>
2024-07-25 22:28:56 +0530

    25-07-2024

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 //const Local_URL = "mongodb://localhost:27017/JiwajiPortal";
-const Live_URL = "mongodb+srv://chaturvediaditya8:Program1ngAP1@ap1express.1qq4hie.mongodb.net/?retryWrites=true&w=majority&appName=JiwajiPortal"
+const Live_URL = "mongodb+srv://chaturvediaditya8:Program1ngAP1@ap1express.1qq4hie.mongodb.net/JiwajiPortal1?retryWrites=true&w=majority&appName=JiwajiPortal"
 const connectDB = () => {
   return mongoose
     .connect(Live_URL)
